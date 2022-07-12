d60aadcb5efd9358667ceebffc21efdaf87280de
Imageumisery <imageumisery@gmail.com>
2022-07-13 00:53:57 +0500

    тт

--- src/main/java/com/example/buysell/controllers/ProductController.java
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -1,12 +1,47 @@
 package com.example.buysell.controllers;
 
+import com.example.buysell.models.Product;
+import com.example.buysell.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String products() {
+    public String products(@RequestParam(name = "title", required = false) String title, Model model) {
+        model.addAttribute("products",productService.productList(title));
         return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo (@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+    }
+
+    @GetMapping("/product/create")
+    public String productInfo (Model model) {
+                return "create";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct (Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
     }
 }
